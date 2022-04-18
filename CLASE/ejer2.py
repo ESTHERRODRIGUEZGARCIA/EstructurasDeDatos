d208a2b46d070e520f108801a50389d861f78cc3
ESTHERRODRIGUEZGARCIA <91721860+ESTHERRODRIGUEZGARCIA@users.noreply.github.com>
2022-04-18 21:11:42 +0200

    importo librerías

--- CLASE/ejer2.py
+++ CLASE/ejer2.py
@@ -5,18 +5,18 @@ siguiendo la filosofía MVC, escriba un programa que lea dos líneas en la entra
 las convierta a mayúsculas y las escriba en un archivo. Tenga en cuenta que para beneficiarse
 plenamente de las ventajas del design pattern MVC, los atributos, en particular los del modelo, se deben encapsular.
 '''
-
+import sys
+import os
 linea1 = "Hola soy Esther"
 linea2 = "Soy una programadora"
 class MVC:
-    def __init__(self, modelo, vista, controlador, linea1, linea2):
-
-        self.modelo = modelo
-        self.vista = vista
-        self.controlador = controlador
+    def __init__(self, linea1, linea2):
         self.linea1 = linea1
         self.linea2 = linea2
     
+    def ejecutar(self):
+        self.controlador.ejecutar()
+
     def leer_lineas(self):
         #quiero que el programa em devuekva las lineas en mayusculas
         def mayusculas(self):
@@ -28,8 +28,7 @@ class MVC:
             return linea1.upper(), linea2.upper()
         return mayusculas(self)
 
-    def ejecutar(self):
-        self.controlador.ejecutar()
+
 
 
 
