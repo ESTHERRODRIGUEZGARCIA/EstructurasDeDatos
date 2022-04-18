b1f42a93230bb86e97fe9945024ded662011a011
ESTHERRODRIGUEZGARCIA <91721860+ESTHERRODRIGUEZGARCIA@users.noreply.github.com>
2022-04-18 20:08:23 +0200

    constructor

--- ejer2.py
+++ ejer2.py
@@ -1,3 +1,13 @@
 # filosofía MVC: Model, View, Controller
+# Modelo:
+'''
+siguiendo la filosofía MVC, escriba un programa que lea dos líneas en la entrada estándar,
+las convierta a mayúsculas y las escriba en un archivo. Tenga en cuenta que para beneficiarse
+plenamente de las ventajas del design pattern MVC, los atributos, en particular los del modelo, se deben encapsular.
+'''
 class MVC:
-    pass
+    def __init__(self, modelo, vista, controlador):
+        self.modelo = modelo
+        self.vista = vista
+        self.controlador = controlador
+        
