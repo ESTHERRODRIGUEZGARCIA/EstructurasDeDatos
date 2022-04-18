a098a1f2c345b6b80c61f3c4912d1904826df54b
ESTHERRODRIGUEZGARCIA <91721860+ESTHERRODRIGUEZGARCIA@users.noreply.github.com>
2022-04-18 21:24:07 +0200

    main cambiado

--- main.py
+++ main.py
@@ -1,20 +1,18 @@
-import CLASE.ejer1 as ejer1
-import CLASE.ejer2 as ejer2
-import CLASE.ejer3 as ejer3
+
 
 
 def elegir():
     variable = input("¿Què ejercicio quiere realizar?: 1, 2 o 3: ")
     if variable == "1":
         print("Ejercicio 1: \n")
-        ejer1()
+        from CLASE import ejer1
     elif variable == "2":
         print("Ejercicio 2: \n")
+        from CLASE import ejer2
 
-        ejer2()
     elif variable == "3":
         print("Ejercicio 3: \n")
-        ejer3()
+        from CLASE import ejer3
     else:
         print("Seleccione una opción correcta. ")
         elegir()
