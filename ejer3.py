6a78866b13026e67a08126340f6eca117b6b7b79
ESTHERRODRIGUEZGARCIA <91721860+ESTHERRODRIGUEZGARCIA@users.noreply.github.com>
2022-04-18 18:12:04 +0200

    naturaleza instancia de la clase

--- ejer3.py
+++ ejer3.py
@@ -31,6 +31,8 @@ class Producto:
             return self.precio_neto
 
 
+Naturaleza = Naturaleza()
+
 producto = Producto(Naturaleza.ALIMENTARIA) # IVA 5,5%
 precio_neto = FactoryFactura.crear(producto).facturar()
 print(precio_neto)
@@ -40,24 +42,4 @@ producto = Producto(Naturaleza.SERVICIO) # IVA 20%
 precio_neto = FactoryFactura.crear(producto).facturar()
 print(precio_neto)
 # 120
-
-class Naturaleza:
-    def __init__(self, naturaleza, iva):
-        self.naturaleza = naturaleza
-        self.ALIMENTARIA = 5.5
-        self.SERVICIO = 20
-        self.iva = iva
-    
-    def __str__(self):
-        return self.naturaleza
-    
-class Producto:
-    def facturar(self):
-        if self.naturaleza == Naturaleza.ALIMENTARIA:
-            return self.precio_neto * 0.055
-        elif self.naturaleza == Naturaleza.SERVICIO:
-            return self.precio_neto * 0.2
-        else:
-            return self.precio_neto
-
 
