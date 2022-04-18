834d463baf6b4e7497579fe3d5caa4efa0065c9d
ESTHERRODRIGUEZGARCIA <91721860+ESTHERRODRIGUEZGARCIA@users.noreply.github.com>
2022-04-18 18:49:35 +0200

    importo ejercicios

---
+++ main.py
@@ -0,0 +1,11 @@
+
+
+def elegir():
+    variable = input("¿Què ejercicio quiere realizar?: 1, 2 o 3: ")
+    if variable == "1":
+        ejer1()
+    elif variable == "2":
+        ejer2()
+    elif variable == "3":
+        ejer3()
+    else:
