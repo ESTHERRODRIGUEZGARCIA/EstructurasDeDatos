431f9c6df610a6ea86377c455e65beaba0c8b6a8
ESTHERRODRIGUEZGARCIA <91721860+ESTHERRODRIGUEZGARCIA@users.noreply.github.com>
2022-04-18 18:01:05 +0200

    primera clase

--- ejer3.py
+++ ejer3.py
@@ -1,2 +1,17 @@
 # Enunciado: Implementar un programa que factura productos por valor de 100, con la tasa de IVA correcta, según sean productos de alimentación o servicios.
+''''
+comportamiento:
+producto = Producto(Naturaleza.ALIMENTARIA) # IVA 5,5% 
+precio_neto = FactoryFactura.crear(producto).facturar() 
+print(precio_neto) 
+# 105.5 
+ 
+producto = Producto(Naturaleza.SERVICIO) # IVA 20% 
+precio_neto = FactoryFactura.crear(producto).facturar() 
+print(precio_neto) 
+# 120 '''
 
+class Naturaleza:
+    ALIMENTARIA = 1
+    SERVICIO = 2
+    
